c6c7a1467aff2ce2d5c80afb2bc5adfa343b83bc
Bryan W. Reid <bwreid@gmail.com>
2013-01-31 18:26:17 -0500

    Sample List

--- main.rb
+++ main.rb
@@ -5,3 +5,13 @@ require_relative 'pets'
 require_relative 'client'
 require_relative 'shelter'
 
+shelter = Shelter.new("HappiTails Shelter")
+
+p1 = Pet.new("Fido", "Terrier", 2, "Male", ["Bones", "Shoes"])
+p2 = Pet.new("Fluffy", "Terrier", 2, "Male", ["Bones", "Shoes"])
+p3 = Pet.new("Sally", "Terrier", 2, "Male", ["Bones", "Shoes"])
+
+c1 = Client.new("Ann", rand(20..50), "female")
+c2 = Client.new("Sarah", rand(20..50), "female")
+c3 = Client.new("Molly", rand(20..50), "female")
+
