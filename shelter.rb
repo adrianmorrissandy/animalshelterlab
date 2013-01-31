fb3da4774eff244951395dd288376c098604e8c1
Adrian Sandy <adrianknowsbest@gmail.com>
2013-01-31 18:17:41 -0500

    pushing shelter

---
+++ shelter.rb
@@ -0,0 +1,15 @@
+require 'pry'
+
+class Shelter
+  attr_accessor :shelter_name :pets :client
+  def initialize(shelter_name)
+  @shelter_name = shelter_name
+  @pets = {}
+  @client = {}
+end
+
+
+def to_s
+"#{shelter_name} is the name"
+end
+end
